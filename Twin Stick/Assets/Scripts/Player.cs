dd7d23de732c8061530b0dd3e501fdd04d2517c2
DRAGON\Dragon <Slayer_thegreat@hotmail.com>
2017-01-21 18:26:04 +1100

    Project updated with new player character, and new replay script.

--- Twin Stick/Assets/Scripts/Player.cs
+++ Twin Stick/Assets/Scripts/Player.cs
@@ -5,6 +5,9 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
+	float y_Position;
+	float z_position;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,8 +16,13 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		Debug.Log (CrossPlatformInputManager.GetAxis ("Horizontal"));
-		Debug.Log (CrossPlatformInputManager.GetAxis ("Vertical"));
+		Debug.Log ("Horizontal: " + CrossPlatformInputManager.GetAxis ("Horizontal"));
+		Debug.Log ("Vertical: " + CrossPlatformInputManager.GetAxis ("Vertical"));
+
+		y_Position = this.transform.position.y;
+		z_position = this.transform.position.z;
+
+		this.transform.position = new Vector3 (0, y_Position, z_position);
 
 	}
 }
---
+++ Twin Stick/Assets/Scripts/ReplaySystem.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaySystem : MonoBehaviour {
+
+	private const int bufferFrames = 100;
+	private MyKeyFrame[] keyFrame = new MyKeyFrame [bufferFrames];
+
+	private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+
+		rigidBody = GetComponent<Rigidbody> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Record ();
+	}
+
+	void PlayBack () {
+		rigidBody.isKinematic = true;
+		int frame = Time.frameCount % bufferFrames;
+		Debug.Log ("Reading to frame: " + frame);
+		transform.position = keyFrame [frame].objectPosition;
+		transform.rotation = keyFrame [frame].objectRotation;
+	}
+
+
+	void Record ()
+	{
+		rigidBody.isKinematic = false;
+		int frame = Time.frameCount % bufferFrames;
+		float time = Time.time;
+		Debug.Log ("Writing to frame: " + frame);
+		keyFrame [frame] = new MyKeyFrame (time, transform.position, transform.rotation);
+	}
+}
+/// <summary>
+/// A "strut" for storing time, position and rotation.
+/// </summary>
+public struct MyKeyFrame {
+	public float time;
+	public Vector3 objectPosition;
+	public Quaternion objectRotation;
+
+	public MyKeyFrame(float atime, Vector3 aobjectPosition, Quaternion aobjectRotation) {
+		time = atime;
+		objectPosition = aobjectPosition;
+		objectRotation = aobjectRotation;
+	}
+
+}
